afee4b9ec24d345119a5b0d942bc06f7e06e9358
prashant-79 <prashantdhote007gmail.com>
2023-01-03 17:41:17 +0530

    adding jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+            sh "aws cloudformation create-stack --stack-name ec2stack --template-body file://ec2_cf_template1.json --region 'ap-south-1'"
+              }
+             }
+            }
+            }
