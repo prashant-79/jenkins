1296f9c5c259e40abb15f84e53b7a86a0cb996eb
prashant-79 <119290289+prashant-79@users.noreply.github.com>
2023-01-04 19:16:17 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Submit Stack') {
             steps {
-            sh "aws cloudformation create-stack --stack-name ec2stack --template-body file://ec2_cf_template1.json --region 'ap-south-1'"
+            bat 'aws cloudformation create-stack --stack-name ec2stack --template-body file://ec2_cf_template1.json --region ap-south-1'
               }
              }
             }
