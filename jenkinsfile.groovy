ea512c62985101a7c20028a85e917fa253b7e26e
prashant-79 <prashantdhote007@gmail.com>
2023-01-04 18:33:32 +0530

    fifth commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,9 +3,7 @@ pipeline {
     stages {
         stage('Submit Stack') {
             steps {
-            bat 'stack-name=ec2stack'
-            bat 'template-body= file://ec2_cf_template1.json'
-            bat'region=ap-south-1'
+            sh "aws cloudformation create-stack --stack-name ec2stack --template-body file://ec2_cf_template1.json --region 'ap-south-1'"
               }
              }
             }
