205532a10a635ff27113721e5a2a3c070c27a6d1
prashant-79 <prashantdhote007@gmail.com>
2023-01-10 15:51:31 +0530

    4th

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,11 @@ pipeline {
     stages {
         stage('Submit Stack') {
             steps {
+<<<<<<< HEAD
             sh 'aws cloudformation --stack-name ec2stack --template-body file://ec2_cf_template1.json --region ap-south-1 --parameters ParameterKey=KeyPairName,ParameterValue=mykey'
+=======
+            bat 'aws cloudformation create-stack --stack-name ec2stack --template-body file://ec2_cf_template1.json --region ap-south-1'
+>>>>>>> 616b5ae (4th)
               }
              }
             }
